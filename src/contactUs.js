efce9fcf6d8688e2710a74c1a050a0c5969cc93b
Daniel Ramirez <dramirtek99@gmail.com>
2022-03-06 20:06:02 -0800

    added input textboxes and their respective labels for the contact us page, still requires styling

--- src/contactUs.js
+++ src/contactUs.js
@@ -5,6 +5,17 @@ const contactUs = () => {
     return (
         <div>
             <h1>Contact Us Page</h1>
+            <body> 
+                <form>
+                    <label for="email">Please Enter Your Email:</label>
+                    <br></br>
+                    <input type="text" id="email" name="email" size="50"></input>
+                    <br></br>
+                    <label for="complaint">Please Specify Your Issue Below:</label>
+                    <br></br>
+                    <input type="text" id="complaint" name="complaint" size="50"></input> 
+                </form>
+            </body>
         </div>
     );
 }
