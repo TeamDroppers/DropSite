84c9031c72e0f89304e73043a486f1120a61263d
Spencer Wallace <007463307@coyote.csusb.edu>
2022-03-17 15:38:58 -0700

    added contact, about, footer, checkout 99%, favorite 75%

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, {useEffect, useState}  from 'react';
 import { Typography, Button, Card, CardActions, CardContent, CardMedia, IconButton } from '@material-ui/core';
 import { Favorite } from '@material-ui/icons'
-
+import { userInfo } from '../../../components-merge/utilites';
 import useStyles from './styles';
 
 const CartItem = ({ item, onUpdateCartQty, onRemoveFromCart, onAddToFavorites }) => {
     const classes = useStyles();
-    console.log(item)
+
+    const [userFavorite, setUserFavorite] = useState(<Empty/>);
+
+    useEffect(() => {
+        fetchUser()
+      }, [])
+
+const fetchUser = async()=>{
+    await userInfo()              
+    .then(user => {
+        if(user.success)
+            setUserFavorite(<FavoriteButton/>);
+        else
+            setUserFavorite(<Empty/>);
+    })
+}
+
+function Empty(){
+    return(
+        <div className="removed"></div>
+    );
+}
+
+function FavoriteButton()
+{
+    return(
+    <IconButton aria-label="Add to Favorites" onClick={() => onAddToFavorites(item.product_id, 1)}>
+        <Favorite />
+    </IconButton>
+    )
+}
+
   return (
     <Card className={classes.itemContainer}>
         <CardMedia image={item.image.url} alt={item.name} className={classes.media} />
@@ -21,9 +52,7 @@ const CartItem = ({ item, onUpdateCartQty, onRemoveFromCart, onAddToFavorites })
                 <Button type="button" size="small" onClick={() => onUpdateCartQty(item.id, item.quantity + 1)}>+</Button>    
             </div>
                 <Button variant="contained" type="button" color="secondary" onClick={() => onRemoveFromCart(item.id)}>Remove</Button> 
-                <IconButton className={classes.favorite} aria-label="Add to Favorites" onClick={() => onAddToFavorites(item.product_id, 1)}>
-                <Favorite />
-                </IconButton>   
+                {userFavorite}
         </CardActions> 
     </Card>
   )
